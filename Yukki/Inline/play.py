024092bab2071b73b60603293a3ea75c4052db22
Tushar Gadhe <80459967+attitudeking1@users.noreply.github.com>
2022-01-08 09:42:04 +0530

    Update play.py

--- Yukki/Inline/play.py
+++ Yukki/Inline/play.py
@@ -47,7 +47,7 @@ def url_markup(videoid, duration, user_id, query, query_type):
         [
             InlineKeyboardButton(
                 text="Kidnap me",
-                url=f"t.me/LOVELYR_OBOT?startgroup=true",
+                url=f"t.me/NANDEDKAR_SHIVAM_BOT?startgroup=true",
             ),
         ],
     ]
@@ -206,7 +206,7 @@ def secondary_markup(videoid, user_id):
         [
             InlineKeyboardButton(
                 text="Kidnap me",
-                url=f"t.me/LOVELYR_OBOT?startgroup=true",
+                url=f"t.me/NANDEDKAR_SHIVAM_BOT?startgroup=true",
             ),
         ],
     ]
@@ -236,7 +236,7 @@ def secondary_markup2(videoid, user_id):
         [
             InlineKeyboardButton(
                 text="Kidnap me",
-                url=f"t.me/LOVELYR_OBOT?startgroup=true",
+                url=f"t.me/NANDEDKAR_SHIVAM_BOT?startgroup=true",
             ),
         ],
     ]
@@ -280,7 +280,7 @@ def primary_markup(videoid, user_id, current_time, total_time):
         [
             InlineKeyboardButton(
                 text="Kidnap me",
-                url=f"t.me/LOVELYR_OBOT?startgroup=true",
+                url=f"t.me/NANDEDKAR_SHIVAM_BOT?startgroup=true",
             ),
         ],
     ]
@@ -320,7 +320,7 @@ def timer_markup(videoid, user_id, current_time, total_time):
         [
             InlineKeyboardButton(
                 text="Kidnap me",
-                url=f"t.me/LOVELYR_OBOT?startgroup=true",
+                url=f"t.me/NANDEDKAR_SHIVAM_BOT?startgroup=true",
             ),
         ],
     ]
@@ -358,7 +358,7 @@ def audio_markup(videoid, user_id, current_time, total_time):
         [
             InlineKeyboardButton(
                 text="Kidnap me",
-                url=f"t.me/LOVELYR_OBOT?startgroup=true",
+                url=f"t.me/NANDEDKAR_SHIVAM_BOT?startgroup=true",
             ),
         ],
     ]
@@ -393,7 +393,7 @@ def audio_timer_markup_start(videoid, user_id, current_time, total_time):
         [
             InlineKeyboardButton(
                 text="Kidnap me",
-                url=f"t.me/LOVELYR_OBOT?startgroup=true",
+                url=f"t.me/NANDEDKAR_SHIVAM_BOT?startgroup=true",
             ),
         ],
     ]
@@ -422,7 +422,7 @@ audio_markup2 = InlineKeyboardMarkup(
         [
             InlineKeyboardButton(
                 text="Kidnap me",
-                url=f"t.me/LOVELYR_OBOT?startgroup=true",
+                url=f"t.me/NANDEDKAR_SHIVAM_BOT?startgroup=true",
             ),
         ],
     ]
